d35110a21c6d1ba6b0f35d38b60f857680a022a1
sthanawa <sthanawa@calpoly.edu>
2017-11-25 21:44:08 -0800

    create s3 bucket for log file in specified location

--- createBuckets/testCreateBuckets.py
+++ createBuckets/testCreateBuckets.py
@@ -24,6 +24,7 @@ def getLocation(num):
     elif num == "11":
         return "sa-east-1";
     elif num == "12":
+        #cannot create a bucket here
         return " "; #if US East then input for login constraints needs to be an empty string
     elif num == "13":
         return "us-east-2";
@@ -49,29 +50,23 @@ s3_resource = boto3.resource(
 
 )
 
-#I think user should be able to indicate location of bucket. If so then we can use this next line:
-log_location_num = input("""
+if (s3_resource.Bucket('capture-log-files') in s3_resource.buckets.all()) :
+    main_bucket = s3_resource.Bucket('Log-files');
+else :
+    # I think user should be able to indicate location of bucket. If so then we can use this next line:
+    log_location_num = input("""
 Where should bucket for log files be located?\n 1 - us-west-1\n 2 - us-west-2\n 3 - eu-west-1
  4 - eu-west-2\n 5 - eu-central-1\n 6 - ap-south-1\n 7 - ap-southeast-1\n 8 - ap-southeast-2\n 9 - ap-northeast-1
  10 - ap-northeast-2\n 11 - sa-east-1\n 12 - US East (N. Virginia) region\n 13 - us-east-2\n
 """);
+    log_location = getLocation(log_location_num);
+    if (log_location != "ERROR"):
+        main_bucket = s3_resource.create_bucket(
+            Bucket='capture-log-files',
+            CreateBucketConfiguration={
+                'LocationConstraint':log_location}
+        );
+    else:
+        print("Not a possible location\n");
 
-
-log_location = getLocation(log_location_num);
-if (log_location != "ERROR"):
-    s3_resource.create_bucket(Bucket='saloneetest',
-                              CreateBucketConfiguration={
-                            'LocationConstraint': log_location});
-else:
-    print("Not a possible location\n");
-
-#bucket = s3_resource.Bucket('temp');
-#print(bucket.name);
-
-#bucket2 = s3_resource.Bucket('temp2');
-#print(bucket2.name);
-
-#testing to see if I can print my bucket names out
-#for bucketInstance in s3_resource.buckets.all():
-#   print(bucketInstance.name);
 
