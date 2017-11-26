cd4df465c5c9fd3da7f3699057446c32db282337
sthanawa <sthanawa@calpoly.edu>
2017-11-25 22:22:17 -0800

    Setup S3 buckets and broken up into functions

--- createBuckets/testCreateBuckets.py
+++ createBuckets/testCreateBuckets.py
@@ -31,45 +31,48 @@ def getLocation(num):
     else:
         return "ERROR";
 
-def createLogBucket():
-    if (s3_resource.Bucket('capture-log-files') in s3_resource.buckets.all()):
-        main_bucket = s3_resource.Bucket('Log-files');
-    else:
-        # I think user should be able to indicate location of bucket. If so then we can use this next line:
-        log_location_num = input("""
-Where should bucket for log files be located?\n 1 - us-west-1\n 2 - us-west-2\n 3 - eu-west-1
+def locationOption():
+    # I think user should be able to indicate location of bucket. If so then we can use this next line:
+    log_location_num = input("""
+Where should bucket be located?\n 1 - us-west-1\n 2 - us-west-2\n 3 - eu-west-1
  4 - eu-west-2\n 5 - eu-central-1\n 6 - ap-south-1\n 7 - ap-southeast-1\n 8 - ap-southeast-2\n 9 - ap-northeast-1
  10 - ap-northeast-2\n 11 - sa-east-1\n 12 - US East (N. Virginia) region\n 13 - us-east-2\n
 """);
-        log_location = getLocation(log_location_num);
-        if (log_location != "ERROR"):
-            main_bucket = s3_resource.create_bucket(
-                Bucket='capture-log-files',
-                CreateBucketConfiguration={
-                    'LocationConstraint': log_location}
-            );
-        else:
-            print("Not a possible location\n");
+    location = getLocation(log_location_num);
+    return location;
+
+def create(name, log_location):
+    s3_resource.create_bucket(
+        Bucket=name,
+        CreateBucketConfiguration={
+            'LocationConstraint': log_location}
+    );
+
+def createLogBucket():
+    if (s3_resource.Bucket('capture-log-files') in s3_resource.buckets.all()):
+        main_log_bucket = s3_resource.Bucket('capture-log-files');
+        print("Capture & Replay bucket already exists");
+    else:
+        print("Creating log bucket");
+        log_location = locationOption();
+        while (log_location == "ERROR"):
+            print("Not a possible location\n")
+            log_location = locationOption();
+        main_log_bucket = create("capture-log-files", log_location);
+    return main_log_bucket;
 
 def createMetricsBucket():
     if (s3_resource.Bucket('metric-files') in s3_resource.buckets.all()):
-        main_bucket = s3_resource.Bucket('metric-files');
+        main_metrics_bucket = s3_resource.Bucket('metric-files');
+        print("Metrics bucket already exists");
     else:
-        # I think user should be able to indicate location of bucket. If so then we can use this next line:
-        log_location_num = input("""
-Where should bucket for metric files be located?\n 1 - us-west-1\n 2 - us-west-2\n 3 - eu-west-1
- 4 - eu-west-2\n 5 - eu-central-1\n 6 - ap-south-1\n 7 - ap-southeast-1\n 8 - ap-southeast-2\n 9 - ap-northeast-1
- 10 - ap-northeast-2\n 11 - sa-east-1\n 12 - US East (N. Virginia) region\n 13 - us-east-2\n
-""");
-        log_location = getLocation(log_location_num);
-        if (log_location != "ERROR"):
-            main_bucket = s3_resource.create_bucket(
-                Bucket='metric-files',
-                CreateBucketConfiguration={
-                    'LocationConstraint': log_location}
-            );
-        else:
-            print("Not a possible location\n");
+        print("Creating metrics bucket");
+        log_location = locationOption();
+        while (log_location == "ERROR"):
+            print("Not a possible location\n")
+            log_location = locationOption();
+        main_metrics_bucket = create("metric-files", log_location);
+    return main_metrics_bucket;
 
 user_key = input("Enter access key: ")
 user_access = input("Enter secret key: ");
@@ -88,8 +91,8 @@ s3_resource = boto3.resource(
     aws_secret_access_key = user_access,
 
 )
-createLogBucket();
-createMetricsBucket();
+main_log_bucket = createLogBucket();
+main_metrics_bucket = createMetricsBucket();
 
 
 
