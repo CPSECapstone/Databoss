52f78481be22b0ea28f3a404bd9737ccf0752c36
sthanawa <sthanawa@calpoly.edu>
2017-11-25 21:49:02 -0800

    Cleaned up code, create 2 s3 buckets

--- createBuckets/testCreateBuckets.py
+++ createBuckets/testCreateBuckets.py
@@ -31,6 +31,45 @@ def getLocation(num):
     else:
         return "ERROR";
 
+def createLogBucket():
+    if (s3_resource.Bucket('capture-log-files') in s3_resource.buckets.all()):
+        main_bucket = s3_resource.Bucket('Log-files');
+    else:
+        # I think user should be able to indicate location of bucket. If so then we can use this next line:
+        log_location_num = input("""
+Where should bucket for log files be located?\n 1 - us-west-1\n 2 - us-west-2\n 3 - eu-west-1
+ 4 - eu-west-2\n 5 - eu-central-1\n 6 - ap-south-1\n 7 - ap-southeast-1\n 8 - ap-southeast-2\n 9 - ap-northeast-1
+ 10 - ap-northeast-2\n 11 - sa-east-1\n 12 - US East (N. Virginia) region\n 13 - us-east-2\n
+""");
+        log_location = getLocation(log_location_num);
+        if (log_location != "ERROR"):
+            main_bucket = s3_resource.create_bucket(
+                Bucket='capture-log-files',
+                CreateBucketConfiguration={
+                    'LocationConstraint': log_location}
+            );
+        else:
+            print("Not a possible location\n");
+
+def createMetricsBucket():
+    if (s3_resource.Bucket('metric-files') in s3_resource.buckets.all()):
+        main_bucket = s3_resource.Bucket('metric-files');
+    else:
+        # I think user should be able to indicate location of bucket. If so then we can use this next line:
+        log_location_num = input("""
+Where should bucket for metric files be located?\n 1 - us-west-1\n 2 - us-west-2\n 3 - eu-west-1
+ 4 - eu-west-2\n 5 - eu-central-1\n 6 - ap-south-1\n 7 - ap-southeast-1\n 8 - ap-southeast-2\n 9 - ap-northeast-1
+ 10 - ap-northeast-2\n 11 - sa-east-1\n 12 - US East (N. Virginia) region\n 13 - us-east-2\n
+""");
+        log_location = getLocation(log_location_num);
+        if (log_location != "ERROR"):
+            main_bucket = s3_resource.create_bucket(
+                Bucket='metric-files',
+                CreateBucketConfiguration={
+                    'LocationConstraint': log_location}
+            );
+        else:
+            print("Not a possible location\n");
 
 user_key = input("Enter access key: ")
 user_access = input("Enter secret key: ");
@@ -49,24 +88,9 @@ s3_resource = boto3.resource(
     aws_secret_access_key = user_access,
 
 )
+createLogBucket();
+createMetricsBucket();
 
-if (s3_resource.Bucket('capture-log-files') in s3_resource.buckets.all()) :
-    main_bucket = s3_resource.Bucket('Log-files');
-else :
-    # I think user should be able to indicate location of bucket. If so then we can use this next line:
-    log_location_num = input("""
-Where should bucket for log files be located?\n 1 - us-west-1\n 2 - us-west-2\n 3 - eu-west-1
- 4 - eu-west-2\n 5 - eu-central-1\n 6 - ap-south-1\n 7 - ap-southeast-1\n 8 - ap-southeast-2\n 9 - ap-northeast-1
- 10 - ap-northeast-2\n 11 - sa-east-1\n 12 - US East (N. Virginia) region\n 13 - us-east-2\n
-""");
-    log_location = getLocation(log_location_num);
-    if (log_location != "ERROR"):
-        main_bucket = s3_resource.create_bucket(
-            Bucket='capture-log-files',
-            CreateBucketConfiguration={
-                'LocationConstraint':log_location}
-        );
-    else:
-        print("Not a possible location\n");
+
 
 
