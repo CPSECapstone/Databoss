b121278944196c7442b7efcc3200567f63a6259b
sthanawa <sthanawa@calpoly.edu>
2017-11-25 19:22:03 -0800

    Added s3 resource and created a temp bucket, name is being printed out

--- createBuckets/testCreateBuckets.py
+++ createBuckets/testCreateBuckets.py
@@ -3,18 +3,27 @@ import boto3
 user_key = input("Enter access key: ")
 user_access = input("Enter secret key: ");
 
-client = boto3.client('s3',
-    aws_access_key_id=user_key,
-    aws_secret_access_key=user_access
-#    region_name='us-west-2'
+#s3 = boto3.client(
+#    service_name='s3',
+#    aws_access_key_id=user_key,
+#    aws_secret_access_key=user_access,
+    #region_name='us-west-1'
+#)
+
+s3_resource = boto3.resource(
+   service_name = 's3',
+    aws_access_key_id = user_key,
+    aws_secret_access_key = user_access
 )
 
 
-#using AWS s3
-s3 = boto3.resource('s3');
+bucket = s3_resource.Bucket('temp');
+print(bucket.name);
+
+#bucket2 = s3_resource.Bucket('temp2');
+#print(bucket2.name);
 
 #testing to see if I can print my bucket names out
-for bucket in s3.buckets.all():
-    print(bucket.name);
-#response = s3.list_buckets();
-#s3.create_bucket(Bucket ='Log Files');
+for bucketInstance in s3_resource.buckets.all():
+   print(bucket.name);
+
