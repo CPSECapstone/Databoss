d5197dda506f88d28da54b02e3a7198f2c6d03cf
Ben Whittle <b.whitty94@gmail.com>
2017-11-27 10:56:26 -0800

    Working on adding connections

--- DBConnection.py
+++ DBConnection.py
@@ -6,7 +6,16 @@ db = None
 
 # Connection format: dialect+driver://username:password@host:port/database
 def addConnection(dialect, user, password, host, database):
-    connection = dialect + "://" + user + ":" + password + "@" + host + "/" \
-                 + database
+    connection = dialect + "://" + user + ":" + password + "@" + host #+ "/" #\
+                 # + database
     print(connection)
     app.config['SQLALCHEMY_DATABASE_URI'] = connection
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.debug = True
+    global db
+    db = SQLAlchemy(app)
+
+
+if __name__ == "__main__":
+    addConnection("mysql+pymysql", "master", "lolmpcsc480", "lolmp.cwaw8hrsce6c.us-west-1.rds.amazonaws.com:3306")
+    print(db.session.execute("show databases;"))
