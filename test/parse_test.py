4c9ac1a1d7e1ebcb312160a5bafa11204c62cfc0
cevenhub <cmnguyen110@gmail.com>
2018-01-21 12:34:58 -0800

    Added a second parse test

--- test/parse_test.py
+++ test/parse_test.py
@@ -1,6 +1,8 @@
 import parseMetrics
+import importlib
 
 file_name_1 = "metric-file.txt"
+file_name_2 = "metric-file-2.txt"
 
 ex_cpu_list_1 = [2.19147911456887, 2.468315272760944, 2.433574140965086, 2.259887005649732, 2.566379549874962, 2.35871075298693, 2.333592664629064, 2.2008057793831517, 2.440677966101698, 2.36607390941929, 2.2341576363804743, 2.2745762711864463]
 ex_cpu_time_list_1 = [1516335600, 1516334100, 1516336200, 1516337100, 1516334400, 1516335000, 1516335900, 1516336500, 1516333800, 1516334700, 1516336800, 1516335300]
@@ -11,8 +13,17 @@ ex_writeIO_time_list_1 = [1516337100, 1516334400, 1516335000, 1516335900, 151633
 ex_memory_list_1 = []
 ex_memory_time_list_1 = []
 
+ex_cpu_list_2 = [2.19147911456887]
+ex_cpu_time_list_2 = [1516335600]
+ex_readIO_list_2 = [0.23336444859314573]
+ex_readIO_time_list_2 = [1516337100]
+ex_writeIO_list_2 = [0.7533527893265134]
+ex_writeIO_time_list_2 = [1516337100]
+ex_memory_list_2 = []
+ex_memory_time_list_2 = []
 
-def test_parse():
+
+def test_parse_1():
     file_data = parseMetrics.readMetricsFile(file_name_1)
     parseMetrics.createCPULists(file_data)
     parseMetrics.createReadIOLists(file_data)
@@ -26,3 +37,20 @@ def test_parse():
     assert parseMetrics.writeIOTimeList == ex_writeIO_time_list_1
     assert parseMetrics.memoryList == ex_memory_list_1
     assert parseMetrics.memoryTimeList == ex_memory_time_list_1
+    importlib.reload(parseMetrics)
+
+def test_parse_2():
+    file_data = parseMetrics.readMetricsFile(file_name_2)
+    parseMetrics.createCPULists(file_data)
+    parseMetrics.createReadIOLists(file_data)
+    parseMetrics.createWriteIOLists(file_data)
+    parseMetrics.createMemLists(file_data)
+    assert parseMetrics.cpuList == ex_cpu_list_2
+    assert parseMetrics.cpuTimeList == ex_cpu_time_list_2
+    assert parseMetrics.readIOList == ex_readIO_list_2
+    assert parseMetrics.readIOTimeList == ex_readIO_time_list_2
+    assert parseMetrics.writeIOList == ex_writeIO_list_2
+    assert parseMetrics.writeIOTimeList == ex_writeIO_time_list_2
+    assert parseMetrics.memoryList == ex_memory_list_2
+    assert parseMetrics.memoryTimeList == ex_memory_time_list_2
+    importlib.reload(parseMetrics)
