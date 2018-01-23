429f6d00497dac2887c752f3a619bfb105b0ede1
Makenna Johnstone <makennajohnstone@Makennas-MacBook-Pro.local>
2018-01-23 11:02:25 -0800

    trying out css

--- static/metrics/metrics.js
+++ static/metrics/metrics.js
@@ -3,5 +3,4 @@ var app = angular.module('MyCRT');
 
 //whenever an action occurs on the metrics page, the controller will handle it
 app.controller('metrics', function($scope, $location, $http) {
-    }
-});
+    });
