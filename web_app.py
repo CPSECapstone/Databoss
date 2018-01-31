5223d2dc1596710342c5c386fc85302cfadc2b66
cevenhub <cmnguyen110@gmail.com>
2018-01-30 21:00:10 -0800

    Changed 'sqlite.py:...' back to 'sqlite:..."

--- web_app.py
+++ web_app.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 # Connect the MyCRT SQLite DB to the Flask app
-sqlite = 'sqlite.py:///database.db'
+sqlite = 'sqlite:///database.db'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = sqlite
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
