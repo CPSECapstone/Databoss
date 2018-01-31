7e21bc7c0566ee001bd69d5070cc00108366f35f
Ben Whittle <b.whitty94@gmail.com>
2018-01-30 22:39:42 -0800

    Modified metrics front-end to support multiple charts and datasets

--- static/metrics/metrics.js
+++ static/metrics/metrics.js
@@ -2,15 +2,36 @@
 var app = angular.module('MyCRT');
 
 //whenever an action occurs on the metrics page, the controller will handle it
-app.controller('metrics', function($scope, $location, $http) {
-    getCPUMetrics($http);
+app.controller('metrics', function($scope, $location, $http, Metrics) {
+//    getMetrics($http);
+
+    Metrics.setCPUChart(createChart('cpuChart', 'CPU (Percent)', 'Time (seconds)'));
+    Metrics.setReadIOChart(createChart('readIOChart', 'Read IO (count/second)', 'Time (seconds)'));
+
+    getMetrics($http, Metrics);
+
 });
 
+var workloadChecked = function() {
+    // TODO get value from checked box and get its corresponding metrics file
+};
+
+var addMetricsToChart = function(chart, label, data, time) {
+    chart.data.labels = time;
+    chart.data.datasets.push({
+        data: data,
+        label: label,
+        borderColor: 'rgba(10, 148, 255, 1)',
+        fill: false
+    });
+    chart.update();
+};
+
 // Function to execute an HTTP request to get CPU Metrics
-var getCPUMetrics = function($http) {
+var getMetrics = function($http, Metrics) {
     $http({
         method: 'GET',
-        url: '/metrics/getCPU',
+        url: '/metrics/getMetrics',
         headers: {
             'Content-Type': 'application/json'
         }
@@ -19,30 +40,48 @@ var getCPUMetrics = function($http) {
         var cpu = response.data.cpu;
         var time = response.data.cpuTime;
         var baseTime = time[0];
+        var readIO = response.data.readIO;
 
         // start the time at 0 seconds
         time = time.map(function(value) {
             return value - baseTime;
         });
 
-        createChart(time, cpu, "CPU");
+        // TODO change capture 1 to response.data.name or some equivalent
+        addMetricsToChart(Metrics.getCPUChart(), 'Capture 1', cpu, time);
+        addMetricsToChart(Metrics.getReadIOChart(), 'Capture 1', readIO, time);
     }, function errorCallback(response) {
         console.log('error');
     });
 };
 
-var createChart = function(labels, data, label) {
-    var ctx = document.getElementById("myChart");
-    var myChart = new Chart(ctx, {
+// Function that creates an empty line chart for a chart element having id=elementId
+// with provided axes labels.
+// Returns the newly created chart
+var createChart = function(elementId, yAxesLabel, xAxesLabel) {
+    var ctx = document.getElementById(elementId);
+    var chart = new Chart(ctx, {
         type: 'line',
         data: {
-            labels: labels,
-            datasets: [{
-                data: data,
-                label: label,
-                borderColor: 'rgba(10, 148, 255, 1)',
-                backgroundColor:'rgba(10, 148, 255, 0.57)'
-            }]
+            datasets: []
+        },
+        options : {
+            scales: {
+                yAxes: [{
+                    scaleLabel: {
+                        display: true,
+                        labelString: yAxesLabel
+                    }
+                }],
+                xAxes: [{
+                    scaleLabel: {
+                        display: true,
+                        labelString: xAxesLabel
+                    }
+                }]
+            }
         }
     });
+
+    return chart;
 };
