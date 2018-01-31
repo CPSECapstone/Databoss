e3b22cbf0ad773136508e145884de0e939e545ae
cevenhub <cmnguyen110@gmail.com>
2018-01-30 20:24:49 -0800

    Reconfigured sqlite to follow flask sqlalchemy
    sqlite.py contents transferred over to models.py. sqlite code
    refactored to use Flask SQL Alchemy syntax. Get functions added for
    capture, replay, and metric and an Add function added for metric.
    
    Tests in sqlite_test.py have been removed for the time being.

--- models.py
+++ models.py
@@ -22,3 +22,114 @@ class DBConnection(db.Model):
             'database': self.database,
             'username': self.username
         }
+
+class metric(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100))
+    path = db.Column(db.String(100), unique=True)
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'path': self.path
+        }
+
+
+class workload(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100))
+    path = db.Column(db.String(100), unique=True)
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'path': self.path
+        }
+
+class logfile(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100))
+    path = db.Column(db.String(100), unique=True)
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'path': self.path
+        }
+
+class capture(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100))
+    time = db.Column(db.DateTime)
+    dbId = db.Column(db.Integer, db.ForeignKey('DBConnection.id'), nullable=False)
+    logfileId = db.Column(db.Integer, db.ForeignKey('logfile.id'), nullable=False)
+    workloadId = db.Column(db.Integer, db.ForeignKey('workload.id'), nullable=False)
+    metricId = db.Column(db.Integer, db.ForeignKey('metric.id'))
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'time': self.time,
+            'dbId': self.dbId,
+            'logfileId': self.logfileId,
+            'workloadId': self.workloadId,
+            'metricId': self.metricId
+        }
+
+class replay(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100))
+    time = db.Column(db.DateTime)
+    dbId = db.Column(db.Integer, db.ForeignKey('DBConnection.id'), nullable=False)
+    logfileId = db.Column(db.Integer, db.ForeignKey('logfile.id'), nullable=False)
+    workloadId = db.Column(db.Integer, db.ForeignKey('workload.id'), nullable=False)
+    metricId = db.Column(db.Integer, db.ForeignKey('metric.id'))
+    captureId = db.Column(db.Integer, db.ForeignKey('capture.id'), nullable=False)
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'time': self.time,
+            'dbId': self.dbId,
+            'logfileId': self.logfileId,
+            'workloadId': self.workloadId,
+            'metricId': self.metricId,
+            'captureId': self.captureId
+        }
+
+# Sets up the tables in the database and their connections. Should be called only once.
+def createTable():
+    db.create_all()
+    db.session.commit()
+
+# Return all captures in the capture table
+def getCaptureAll():
+    capList = capture.query.with_entities(capture.id, capture.name, capture.time)
+    return capList
+
+# Return all replays in the replay table
+def getReplayAll():
+    repList = replay.query.with_entities(replay.id, replay.name, replay.time)
+    return repList
+
+# Add metric to the metric table
+def addMetric(name, path):
+    newMetric = metric(name, path)
+    db.session.add(newMetric)
+    db.session.commit()
+
+# Return metric associated with provided capture or replay
+def getMetric(metricId):
+    m = metric.query.filter_by(id=metricId).all()
+    return m
+
