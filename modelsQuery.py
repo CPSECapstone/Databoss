0416776d6aefba1c0c7c5f25d16d939d5946fe54
cevenhub <cmnguyen110@gmail.com>
2018-02-05 19:34:50 -0800

    Model init and queries
    Model init functions setup and changes made from time to be startTime
    and endTime

--- modelsQuery.py
+++ modelsQuery.py
@@ -1,31 +1,30 @@
 import models
 
-
 # Sets up the tables in the database and their connections. Should be called only once.
 def createTable():
     models.db.create_all()
     models.db.session.commit()
 
 # Add capture to capture table with references to associated files
-def addCapture(name, time, dbId, logfileId, metricId):
-    new_cap = models.Capture(name, time, dbId, logfileId, metricId)
+def addCapture(name, startTime, endTime, dbId, logfileId, metricId):
+    new_cap = models.Capture(name, startTime, endTime, dbId, logfileId, metricId)
     models.db.session.add(new_cap)
     models.db.session.commit()
 
 # Return all captures in the capture table
 def getCaptureAll():
-    cap_list = models.Capture.query.with_entities(models.Capture.id, models.Capture.name, models.Capture.time)
+    cap_list = models.Capture.query.with_entities(models.Capture.id, models.Capture.name, models.Capture.startTime)
     return cap_list
 
 # Add replay to replay table with references to associated files
-def addReplay(name, time, dbId, logfileId, metricId, captureId):
-    new_rep = models.Capture(name, time, dbId, logfileId, metricId, captureId)
+def addReplay(name, startTime, endTime, dbId, logfileId, metricId, captureId):
+    new_rep = models.Replay(name, startTime, endTime, dbId, logfileId, metricId, captureId)
     models.db.session.add(new_rep)
     models.db.session.commit()
 
 # Return all replays in the replay table
 def getReplayAll():
-    rep_list = models.Replay.query.with_entities(Replay.id, Replay.name, Replay.time)
+    rep_list = models.Replay.query.with_entities(models.Replay.id, models.Replay.name, models.Replay.startTime)
     return rep_list
 
 # Add metric to the metric table
