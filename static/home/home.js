c5456a4d978c1d2246911870dcdd2e1760418eda
Makenna Johnstone <makennajohnstone@makennas-mbp.home>
2018-02-21 17:52:15 -0800

    added capture table on the home page

--- static/home/home.js
+++ static/home/home.js
@@ -1,11 +1,29 @@
 //Initialize the angular application for this AngularJS controller
 var app = angular.module('MyCRT');
 
-app.controller('home', function($scope, $location) {
+app.controller('home', function($scope, $location, $http) {
         console.log("I'm home")
     $scope.goCapture = function () {
         console.log("HERE")
         $location.path('/capture');
     }
    $('.datepicker').datepicker();
+
+   populateCaptures($http, $scope);
+
 });
+
+var populateCaptures = function($http, $scope) {
+    $http({
+        method: 'GET',
+        url: 'capture/getAll',
+        headers: {
+        'Content-Type': 'application/json'
+        },
+    }).then(function successCallback(response) {
+        $scope.captures = response.data;
+        console.log('success');
+    }, function errorCallback(response) {
+        console.log('error retrieving captures');
+    })
+};
