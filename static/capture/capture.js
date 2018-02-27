d0bd8fcb997857d22258640e0732c8224191f429
Makenna Johnstone <makennajohnstone@makennas-mbp.home>
2018-02-26 19:58:00 -0800

    alright fixed some tragic copy and pastes

--- static/capture/capture.js
+++ static/capture/capture.js
@@ -1,56 +1,172 @@
-from capture import capture_api
-from flask import jsonify, request
-from capture import startCapture, stopCapture
-from models import Capture
-import modelsQuery
-from models import Capture
+//Initialize the angular application for this AngularJS controller
+var app = angular.module('MyCRT');
 
-@capture_api.route('/<name>')
-def getCapture(name):
-    capture = modelsQuery.getCaptureByName(name)
-    return jsonify(capture.serialize)
+//app.controller('capture', ['$scope', '$location', '$modal', function($scope, $location, $modal) {
+app.controller('capture', function ($scope, $location, $uibModal, $http) {
+    console.log("in capture");
 
-@capture_api.route('/getAll')
-def getAllCaptures():
-    captures = Capture.query.all()
-    return jsonify([i.serialize for i in captures])
-
-def add(name, startTime, endTime, dbName, logfileId, metricId, mode):
-    modelsQuery.addCapture(name, startTime, endTime, dbName, logfileId, metricId, mode)
+    // setting variables
+    const captureModeBar = document.getElementById('capture-mode-bar');
+    const dateContainer = $('#date-container');
+    const timeContainer = $('#time-container');
+    const storageContainer = $('#storage-container');
+    var selectedMode = "";
 
 
-@capture_api.route('/startCapture', methods=["POST"])
-def captureRoute():
-    data = request.json
+    //setting an observer for the captureModeBar to change input view when clicked
+    const hideButtons = function() {
+      console.log(arguments);
+      for (var i = 0; i < arguments.length; i++) {
+        arguments[i].hide();
+      }
+    }
 
-    captureName = data['captureName']
-    captureBucket = data['captureBucket']
-    metricsBucket = data['metricsBucket']
-    dbName = data['dbName']
-    startDate = data['startDate']
-    endDate = data['endDate']
-    startTime = data['startTime']
-    endTime = data['endTime']
-    mode = data['mode']
-    print("THIS IS DA MODE" + mode)
+    const showButtons = function() {
+      console.log(arguments);
+      for (var i = 0; i < arguments.length; i++) {
+        arguments[i].show();
+      }
+    }
 
-    print(data)
-    startCapture(captureName, captureBucket, metricsBucket, dbName, startDate, endDate, startTime, endTime, None, mode)
-    return "ok"
+    //setup
+    hideButtons(dateContainer, timeContainer, storageContainer);
 
-@capture_api.route('/endCapture', methods=["POST"])
-def endCapture():
-    data = request.json
-    print(data)
-    captureName = data.get('name')
-    dbName = data.get('dbName')
+    $('input[name=mode]').on('change', function(event) {
+      selectedMode = $("input[name=mode]:checked").val();
+      console.log("value " + selectedMode);
+      if (selectedMode === "interactive") {
+        console.log("updating to interactive view");
+        hideButtons(dateContainer, timeContainer, storageContainer);
+      }
+      else if (selectedMode === "time") {
+        console.log("updating to time constrained view");
+        showButtons(dateContainer, timeContainer);
+        hideButtons(storageContainer);
+      }
+      else if (selectedMode === "storage") {
+        console.log("updating to storage view");
+        hideButtons(dateContainer, timeContainer);
+        showButtons(storageContainer);
+      }
+      else {
+        console.log("NO MODE SELECTED");
+      }
+    });
 
-    startTime = data.get('startTime')
-    endTime = data.get('endTime')
-    captureBucket = data.get('captureBucket')
-    metricBucket = data.get('metricBucket')
-    captureFileName = data.get('captureFileName')
-    metricFileName = data.get('metricFileName')
-    mode = data.get('mode')
+    $scope.open = function () {
+        console.log('opening pop up');
+        var modalInstance = $uibModal.open({
+            templateUrl: '/static/capture/addDatabaseModal.html',
+            controller: function ($scope, $uibModalInstance) {
+                $scope.add = function () {
+                    $http({
+                        method: 'POST',
+                        url: '/dbc/add',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        data: {
+                            'name': $scope.name,
+                            'host': $scope.host,
+                            'port': $scope.port,
+                            'username': $scope.username,
+                            'password': $scope.password,
+                            'database': $scope.database
+                        }
+                    }).then(function successCallback(response) {
+                        console.log('success');
+                    }, function errorCallback(response) {
+                        console.log('error');
+                    });
 
-    stopCapture(startTime, endTime, captureName, captureBucket, metricBucket, captureFileName, metricFileName)
+                    $uibModalInstance.close();
+                };
+
+                $scope.cancel = function () {
+                    $uibModalInstance.dismiss('cancel');
+                };
+            }
+        });
+
+        modalInstance.result.then(function() {
+            console.log("do we get here?")
+            $scope.getDBConnections();
+        });
+    };
+
+    $scope.getDBConnections = function() {
+        console.log("getting db connections");
+
+        $http({
+            method: 'GET',
+            url: 'capture/listDBinstances',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        }).then(function successCallback(response) {
+            $scope.DBConnections = response.data;
+            console.log('success');
+        }, function errorCallback(response) {
+            console.log('error');
+        });
+    };
+
+    $scope.getDBConnections();
+
+    var getBuckets = function() {
+        $http({
+            method: 'GET',
+            url: 'capture/listbuckets',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        }).then(function successCallback(response) {
+            $scope.buckets = response.data;
+            console.log('success');
+        }, function errorCallback(response) {
+            console.log('error');
+        });
+    };
+
+    getBuckets();
+
+    $scope.startCapture = function () {
+        $http({
+            method: 'POST',
+            url: 'capture/startCapture',
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            data : {
+                'captureName' : $('#captureName').val(),
+                'captureBucket' : $('#crBucket').val(),
+                'metricsBucket' : $('#metricsBucket').val(),
+                'dbName' : $('#dbName').val(),
+                'startDate' : $('#startDate').val(),
+                'endDate' : $('#endDate').val(),
+                'startTime' : $('#startTime').val(),
+                'endTime' : $('#endTime').val(),
+                'mode' : $('input[name=mode]:checked').val()
+
+                // 'storageLimit' : $('#')
+                //unsure how to grab the value of the storage limit.
+            }
+        });
+
+        $location.path('progress').search({name : $('#captureName').val()});
+    }
+
+    $scope.setStorageSize = function (id) {
+      console.log(id);
+      //clear active
+      if (id === "mb-button") {
+        document.getElementById(id).classList.add('active');
+        document.getElementById('gb-button').classList.remove('active');
+      }
+      else {
+        document.getElementById(id).classList.add('active');
+        document.getElementById('mb-button').classList.remove('active');
+      }
+    }
+});
+
