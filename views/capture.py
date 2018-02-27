cd66175161110990969753132b8a14a85de804fd
sonianara <sonaraya@calpoly.edu>
2018-02-26 18:03:19 -0800

    fixed capture interactive mode

--- views/capture.py
+++ views/capture.py
@@ -32,7 +32,7 @@ def captureRoute():
     endTime = data['endTime']
 
     startCapture(captureName, captureBucket, metricsBucket, dbName, startDate, endDate, startTime, endTime, None)
-    return
+    return "ok"
 
 @capture_api.route('/endCapture', methods=["POST"])
 def endCapture():
