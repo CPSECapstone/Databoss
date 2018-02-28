ab62e3982f63d499d68e62de8b515d399ae2fd56
sthanawa <sthanawa@calpoly.edu>
2018-02-28 13:40:27 -0800

    created ec2 instance with correct instance profile

--- authentication.py
+++ authentication.py
@@ -9,53 +9,42 @@ import json
 import os.path
 from boto3 import Session
 
-#import json
-#import os.path
+# TODO: remove once hosted on ec2 instance
+if os.path.exists("credentials.json"):
+    credentialFile = open("credentials.json", "r")
+    credentials = json.load(credentialFile)
+    access_key = credentials['access']
+    secret_key = credentials['secret']
 
-#if os.path.exists("credentials.json"):
-#    credentialFile = open("credentials.json", "r")
-#    credentials = json.load(credentialFile)
-#    access_key = credentials['access']
-#    secret_key = credentials['secret']
-
-#iam_resource = boto3.resource(
-#        service_name='s3',
-#        aws_access_key_id=access_key,
-#        aws_secret_access_key=secret_key,
-#        region_name = "us-west-1"
-#)
-
-#def setupUser
+#access key and secret key only needed for testing on local machine
+ec2_resource = boto3.resource(
+                service_name='ec2',
+                aws_access_key_id=access_key,
+                aws_secret_access_key=secret_key,
+                region_name = 'us-west-1'
+)
 
 
-#ec2_resource = boto3.resource(
-#                service_name='ec2',
-#                region_name = 'us-west-1'
-#)
+#clientIam = boto3.client(service_name='iam')
+#clientSts = boto3.client(service_name='sts')
+#iam = boto3.resource('iam')
 
+#tokenDuration = 86400 # duration in seconds
+#region = 'us-west-1c'
 
+#def loginUser(username, password):
+#    session = boto3.Session(aws_access_key_id=access_key, aws_secret_access_key=secret_key, region_name=region)
+#    credentials = session.get_credentials()
+#    credentials = credentials.get_frozen_credentials()
+   #access_key = credentials.access_key
+   #secret_key = credentials.secret_key
+#    return credentials
 
-#instance_profile = iam.InstanceProfile('attempt2')
-
-
-#ec2_client.create_instances('try1')
-
-#for instance in ec2_resource.instances.all():
-#    print(instance.id)
-
-clientIam = boto3.client(service_name='iam')
-clientSts = boto3.client(service_name='sts')
-iam = boto3.resource('iam')
-
-tokenDuration = 86400 # duration in seconds
-accessKey = '' # Place access key
-secretKey = '' # Place secret key
-region = 'us-west-1'
-
-def loginUser(username, password):
-    session = boto3.Session(aws_access_key_id=accessKey, aws_secret_access_key=secretKey, region_name=region)
-    credentials = session.get_credentials()
-    credentials = credentials.get_frozen_credentials()
-    #access_key = credentials.access_key
-    #secret_key = credentials.secret_key
-    return credentials
+#this will attach the user's required iam instance profile (which should have the proper permissions) to an ec2 instance
+def login(imageId, ipName):
+    ec2_resource.create_instances(ImageId='ami-327f5352', #should be replaced with imageId
+                     InstanceType='t2.micro',
+                     MinCount=1, MaxCount=1,
+                     IamInstanceProfile={
+                            'Name': ipName
+                     })
