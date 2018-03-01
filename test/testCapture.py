6ce5acc06343eed267e0104dd76d4e5c71193f3c
Ben Whittle <b.whitty94@gmail.com>
2018-02-28 20:55:43 -0800

    Travis with AWS Credentials round 6

--- test/testCapture.py
+++ test/testCapture.py
@@ -5,20 +5,8 @@ import pytest
 import json
 import os.path
 
-access_key = None
-secret_key = None
 
-
-if os.path.exists("../credentials.json"):
-    credentialFile = open("../credentials.json", "r")
-    credentials = json.load(credentialFile)
-    access_key = credentials['access']
-    secret_key = credentials['secret']
-    capture.aws_config(access_key, secret_key)
-    testStorage.aws_config(access_key, secret_key)
-else:
-    print("ERROR: could not find credentials")
-
+loc = "us-west-1"
 s3 = boto3.client(
         service_name='s3'
     )
@@ -26,15 +14,39 @@ s3_resource = s3 = boto3.resource(
         service_name='s3'
     )
 
-def test_BucketCreation(bucketName):
+rds = boto3.client(
+        service_name='rds',
+        region_name=loc
+    )
+
+bucketName = 'capture-test-13'
+# def pytest_namespace():
+#     return {'bucketName': 'capture-test-6'}
+#
+# @pytest.fixture
+# def data():
+#     pytest.bucketName = 'capture-test-6'
+#
+# @pytest.fixture
+# def initializeBoto():
+#     s3 = boto3.client(
+#         service_name='s3'
+#     )
+#     s3_resource = s3 = boto3.resource(
+#         service_name='s3'
+#     )
+
+
+def test_BucketCreation():
     bucket_list = [bucket.name for bucket in s3.buckets.all()]
-    initialSize = bucket_list.__len__()
     capture.createBucket(bucketName)
     bucket_list = [bucket.name for bucket in s3.buckets.all()]
     sizeAfterAdd = bucket_list.__len__()
     assert len(bucket_list) == sizeAfterAdd
 
-def test_BucketExists(bucketName):
+
+
+def test_BucketExists():
     bucket_list = [bucket.name for bucket in s3.buckets.all()]
     initialSize = bucket_list.__len__()
     capture.createBucketName(bucketName, bucketName)
@@ -42,17 +54,19 @@ def test_BucketExists(bucketName):
     sizeAfterAdd = bucket_list.__len__()
     assert (initialSize == sizeAfterAdd)
 
-def test_StorageMax(size, db_name):
-    testStorage.startRDS(db_name, "sthanawa", "sthanawa", "storagedb.coircswctb4r.us-west-1.rds.amazonaws.com")
-    res = testStorage.checkStorageCacity(size, db_name)
-    assert (res == -1)
+# def test_StorageMax():
+#     size = 5
+#     db_name = 'testing'
+#     testStorage.startRDS(db_name, "master", "password", "testing.ckunormkfqio.us-west-1.rds.amazonaws.com")
+#     res = testStorage.checkStorageCacity(size, db_name)
+#     assert (res == -1)
 
-def test_Storage(size, db_name):
-    testStorage.startRDS(db_name, "sthanawa", "sthanawa", "storagedb.coircswctb4r.us-west-1.rds.amazonaws.com")
-    res = testStorage.checkStorageCacity(size, db_name)
-    assert (res == 0)
-
-test_BucketCreation('capture-test-6')
-test_BucketExists('capture-test-6')
-#test_StorageMax(5, 'storagedb')
-#test_Storage(25, 'storagedb')
+# def test_Storage(size, db_name):
+#     testStorage.startRDS(db_name, "sthanawa", "sthanawa", "storagedb.coircswctb4r.us-west-1.rds.amazonaws.com")
+#     res = testStorage.checkStorageCacity(size, db_name)
+#     assert (res == 0)
+#
+# test_BucketCreation('capture-test-6')
+# test_BucketExists('capture-test-6')
+# #test_StorageMax(5, 'storagedb')
+# #test_Storage(25, 'storagedb')
