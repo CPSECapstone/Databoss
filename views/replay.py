e6e3f7302ab414573185c83845f510177fadf4ca
sonianara <sonaraya@calpoly.edu>
2018-03-12 18:03:05 -0700

    removing uncommented code

--- views/replay.py
+++ views/replay.py
@@ -17,6 +17,5 @@ def add(name, startTime, endTime, dbName, logfileId, metricId, captureId):
 @replay_api.route('/startReplay', methods=["POST"])
 def startReplay():
     data = request.json
-    print(data)
     replay.startReplay(data['replayName'], data['capture'], data['dbName'], data['replayMode'], data['username'], data['password'])
     return ""
