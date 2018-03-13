51f361a686ddf9263a6a7d0a47f6d4c379059528
sonianara <sonaraya@calpoly.edu>
2018-03-12 17:59:42 -0700

    adding replay functionality

--- views/replay.py
+++ views/replay.py
@@ -17,5 +17,6 @@ def add(name, startTime, endTime, dbName, logfileId, metricId, captureId):
 @replay_api.route('/startReplay', methods=["POST"])
 def startReplay():
     data = request.json
+    print(data)
     replay.startReplay(data['replayName'], data['capture'], data['dbName'], data['replayMode'], data['username'], data['password'])
     return ""
