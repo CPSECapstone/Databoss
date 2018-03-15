969d67f44daa230c3e743607aadedb90452da315
sonianara <sonaraya@calpoly.edu>
2018-03-14 17:04:55 -0700

    pushing replay changes

--- views/replay.py
+++ views/replay.py
@@ -17,6 +17,7 @@ def add(name, startTime, endTime, dbName, logfileId, metricId, captureId):
 @replay_api.route('/startReplay', methods=["POST"])
 def startReplay():
     data = request.json
+    print(data)
     replay.startReplay(data['replayName'], data['capture'], data['dbName'], data['replayMode'], data['username'], data['password'])
     return ""
 
