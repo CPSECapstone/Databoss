1b1d568bd99e14677e73673a7602704fddec8f7a
Ben Whittle <b.whitty94@gmail.com>
2018-04-26 08:42:19 -0700

    Filtered out rds queries from capture file and fixed replay connection error

--- replay.py
+++ replay.py
@@ -85,7 +85,7 @@ def startReplay(replayName, captureObj, dbName, mode, username, password):
 
 def executeReplay(replayName, captureName, dbName, status_of_db, endpoint, metricFile, startTime):
     print("capture name here: " + captureName)
-    print("db name: "  + dbName)
+    print("db name: " + dbName)
     metricBucket = modelsQuery.getCaptureMetricBucket(captureName)
     inProgressReplay = getInProgressReplay(replayName)
     username = inProgressReplay.get('username')
@@ -94,6 +94,16 @@ def executeReplay(replayName, captureName, dbName, status_of_db, endpoint, metri
     numQueriesExecuted = 0
 
     with open(captureName + " " + "tempLogFile", 'r') as tempFile:
+        try:
+            conn = pymysql.connect(host=endpoint, user=username, passwd=password, db=dbName,
+                                   connect_timeout=5)
+        except:
+            logger.error("ERROR: Unexpected error: Could not connect to MySql instance.")
+            if os.path.exists(captureName + " " + "tempLogFile"):
+                os.remove(captureName + " " + "tempLogFile")
+            conn.close()
+            sys.exit()
+
         for line in tempFile:
             entireList = literal_eval(line)
             for i in range(len(entireList)):
@@ -101,12 +111,6 @@ def executeReplay(replayName, captureName, dbName, status_of_db, endpoint, metri
                 if dict['message'].startswith('Query'):
                     executableQuery = dict['message'][7:]
                     if str(status_of_db) == "available":
-                        try:
-                            conn = pymysql.connect(host=endpoint, user=username, passwd=password, db=dbName,
-                                                   connect_timeout=5)
-                        except:
-                            logger.error("ERROR: Unexpected error: Could not connect to MySql instance.")
-                            sys.exit()
                         with conn.cursor() as cur:
                             try:
                                 cur.execute(executableQuery)
@@ -125,3 +129,5 @@ def executeReplay(replayName, captureName, dbName, status_of_db, endpoint, metri
     modelsQuery.updateReplayStatus(replayName, "finished")
     metricID = modelsQuery.getMetricIDByNameAndBucket(replayName + " " + "metric file", metricBucket)
     capture.sendMetrics(metricID, replayName + " " + "metric file", startTime, endTime)
+
+    conn.close()
