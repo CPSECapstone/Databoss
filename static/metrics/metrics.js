ae424f20a2c09a5a3265536eac4c44160bfa5137
Makenna Johnstone <makennajohnstone@gmail.com>
2018-05-10 09:38:34 -0700

    retrieving capture buckets and RDS instance for metrics table

--- static/metrics/metrics.js
+++ static/metrics/metrics.js
@@ -286,14 +286,16 @@ var createChart = function(elementId, yAxesLabel, xAxesLabel) {
 var getCaptures = function($http, $scope) {
     $http({
         method: 'GET',
-        url: 'capture/getAll',
+        url: 'capture/getCapturesWithBuckets',
         headers: {
         'Content-Type': 'application/json'
         },
     }).then(function successCallback(response) {
-        $scope.captures = response.data.filter(capture =>
-          capture.status === "finished");;
-        console.log('success');
+        console.log(response.data);
+        $scope.captures = response.data;
+//        $scope.captures = response.data.filter(capture =>
+//            capture.status == "finished");
+
     }, function errorCallback(response) {
         console.log('error retrieving captures');
     })
