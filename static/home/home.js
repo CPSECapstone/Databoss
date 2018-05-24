e235cdcc568a1b12d53eac4945861cdbe5315218
Kim Tan <kimmytan.93@gmail.com>
2018-05-23 20:37:33 -0700

    Refactored stuff into the wrap method for WELC pattern applied. Also fixed some indentation

--- static/home/home.js
+++ static/home/home.js
@@ -11,23 +11,23 @@ app.controller('home', function($scope, $location, $http, activeNavItem) {
   }
 
   $scope.goCapture = function () {
-      $location.path('/capture');
+    $location.path('/capture');
   }
 
   $scope.promptDelete = function(item) {
     if (item) {
-        $scope.itemName = item.name;
-        $scope.itemObj = item;
-        $('#deleteModal').modal('show');
+      $scope.itemName = item.name;
+      $scope.itemObj = item;
+      $('#deleteModal').modal('show');
     }
   }
 
   $scope.deleteItem = function(item) {
     if (item.type == "capture") {
-        this.deleteCapture(item.id)
+      this.deleteCapture(item.id)
     }
     else if (item.type == "replay") {
-        this.deleteReplay(item.id)
+      this.deleteReplay(item.id)
     }
     $('#deleteModal').modal('hide');
   }
@@ -42,35 +42,35 @@ app.controller('home', function($scope, $location, $http, activeNavItem) {
 
   $scope.deleteCapture = function(captureId) {
     $http({
-        method: 'DELETE',
-        url: 'capture/deleteCapture/' + captureId,
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        data: {
-            'captureId': captureId
-        }
+      method: 'DELETE',
+      url: 'capture/deleteCapture/' + captureId,
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      data: {
+        'captureId': captureId
+      }
     }).then(function successCallback(response) {
-        populateFinishedCapturesAndReplays($http, $scope);
+      populateFinishedCapturesAndReplays($http, $scope);
     }, function errorCallback(response) {
-        console.log('error');
+      console.log('error');
     });
   }
 
   $scope.deleteReplay = function(replayId) {
     $http({
-        method: 'DELETE',
-        url: 'replay/deleteReplay/' + replayId,
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        data: {
-            'replayId': replayId
-        }
+      method: 'DELETE',
+      url: 'replay/deleteReplay/' + replayId,
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      data: {
+        'replayId': replayId
+      }
     }).then(function successCallback(response) {
-        populateFinishedCapturesAndReplays($http, $scope);
+      populateFinishedCapturesAndReplays($http, $scope);
     }, function errorCallback(response) {
-        console.log('error');
+      console.log('error');
     });
   }
 
@@ -91,31 +91,29 @@ app.controller('home', function($scope, $location, $http, activeNavItem) {
   }
 
   $scope.filterAll = function() {
-    $("#filter-captures").removeClass('active');
-    $("#filter-replays").removeClass('active');
-    $("#filter-all").addClass('active');
-    $scope.showAll = true;
-    $scope.showCaptures = false;
-    $scope.showReplays = false;
-  }
-
-  $scope.filterCaptures = function() {
-    $("#filter-captures").addClass('active');
-    $("#filter-replays").removeClass('active');
-    $("#filter-all").removeClass('active');
-    $scope.showAll = false;
-    $scope.showCaptures = true;
-    $scope.showReplays = false;
+    this.filter(['#filter-all'], ['#filter-replays','#filter-captures'], true, false, false);
   }
 
   $scope.filterReplays = function() {
-    $("#filter-replays").addClass('active');
-    $("#filter-captures").removeClass('active');
-    $("#filter-all").removeClass('active');
-    $scope.showAll = false;
-    $scope.showCaptures = false;
-    $scope.showReplays = true;
+    this.filter(['#filter-replays'], ['#filter-captures','#filter-all'], false, false, true);
   }
+
+  $scope.filterCaptures = function() {
+    this.filter(['#filter-captures'], ['#filter-replays','#filter-all'], false, true , false);
+  }
+
+  $scope.filter = function(activeIds, inactiveIds, showAll, showCaptures, showReplays) {
+    for (activeId in activeIds) {
+      $(activeIds[activeId]).addClass('active');
+    }
+    for (inactiveId in inactiveIds) {
+      $(inactiveIds[inactiveId]).removeClass('active');
+    }
+    $scope.showAll = showAll;
+    $scope.showCaptures = showCaptures;
+    $scope.showReplays = showReplays;
+  }
+
 
   populateFinishedCapturesAndReplays($http, $scope);
   populateActiveCaptures($http, $scope);
@@ -133,75 +131,75 @@ var options = {
 
 var populateFinishedCapturesAndReplays = function($http, $scope) {
   $http({
-        method: 'GET',
-        url: 'capture/getSortedCapturesAndReplays',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-    }).then(function successCallback(response) {
-      $scope.active = [];
-      $scope.finished = [];
-      response.data.captures.map((capture) => {
-        capture.type = "capture";
-        if (capture.status == "active") {
-          $scope.active.push(capture);
-        }
-        else if (capture.status == "finished") {
-          capture.passFail = "passed";
-          $scope.finished.push(capture);
-        } else if (capture.status == "failed") {
-          capture.passFail = "failed";
-          $scope.finished.push(capture);
-        }
-      })
-
-      response.data.replays.map((replay) => {
-        replay.type = "replay";
-        if (replay.status == "active") {
-          replay.progress = " ";
-          $scope.active.push(replay);
-        }
-        else if (replay.status == "finished") {
-          replay.passFail = "passed";
-          $scope.finished.push(replay);
-        }
-      })
-      formatDates($scope.finished);
-      formatDates($scope.active);
-      calculateProgress($scope.active);
-    }, function errorCallback(response) {
-        console.error('error retrieving captures');
+    method: 'GET',
+    url: 'capture/getSortedCapturesAndReplays',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  }).then(function successCallback(response) {
+    $scope.active = [];
+    $scope.finished = [];
+    response.data.captures.map((capture) => {
+      capture.type = "capture";
+      if (capture.status == "active") {
+        $scope.active.push(capture);
+      }
+      else if (capture.status == "finished") {
+        capture.passFail = "passed";
+        $scope.finished.push(capture);
+      } else if (capture.status == "failed") {
+        capture.passFail = "failed";
+        $scope.finished.push(capture);
+      }
     })
+
+    response.data.replays.map((replay) => {
+      replay.type = "replay";
+      if (replay.status == "active") {
+        replay.progress = " ";
+        $scope.active.push(replay);
+      }
+      else if (replay.status == "finished") {
+        replay.passFail = "passed";
+        $scope.finished.push(replay);
+      }
+    })
+    formatDates($scope.finished);
+    formatDates($scope.active);
+    calculateProgress($scope.active);
+  }, function errorCallback(response) {
+    console.error('error retrieving captures');
+  })
 };
 
 var populateActiveCaptures = function($http, $scope) {
-    $http({
-        method: 'GET',
-        url: 'capture/active',
-        headers: {
-        'Content-Type': 'application/json'
-        },
-    }).then(function successCallback(response) {
-        $scope.activeCaptures = response.data;
-        calculateProgress($scope.activeCaptures);
-    }, function errorCallback(response) {
-        console.log('error retrieving captures');
-    })
+  $http({
+    method: 'GET',
+    url: 'capture/active',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  }).then(function successCallback(response) {
+    $scope.activeCaptures = response.data;
+    calculateProgress($scope.activeCaptures);
+  }, function errorCallback(response) {
+    console.log('error retrieving captures');
+  })
 };
 
 var populateScheduledCaptures = function($http, $scope) {
-    $http({
-        method: 'GET',
-        url: 'capture/scheduled',
-        headers: {
-        'Content-Type': 'application/json'
-        },
-    }).then(function successCallback(response) {
-        $scope.scheduledCaptures= response.data;
-        formatDates($scope.scheduledCaptures);
-    }, function errorCallback(response) {
-        console.log('error retrieving captures');
-    })
+  $http({
+    method: 'GET',
+    url: 'capture/scheduled',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  }).then(function successCallback(response) {
+    $scope.scheduledCaptures= response.data;
+    formatDates($scope.scheduledCaptures);
+  }, function errorCallback(response) {
+    console.log('error retrieving captures');
+  })
 };
 
 var formatDates = function(captures) {
